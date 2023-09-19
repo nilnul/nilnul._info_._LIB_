5d598fb0b28838b12aaea0c589311abe2c9453c2
WANG,Youtian <wangyoutian@nilnul.com>
2023-09-20 00:41:19 +0800

    restructure for info definitions; csproj file is updated; etc;

--- nilnul0/_info/_indefinite/proffer/IMeasure.cs
+++ nilnul0/_info/_indefinite/proffer/IMeasure.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace nilnul._info._indefinite.proffer
 {
-	/// <summary>
 	/// define a finite (not infinite) nontrivia (not all zeros) measure.
+	/// <summary>
+	/// given n states,
+	/// the measure has to be nonneg.
+	///		U(n) being nonneg
+	///	has to be sigma additative:
+	///		U(n*m) =U(n) + U(m), as choosing from n*m can be divided into two steps: choose group out of n, then choose member out m.
+	///	we further need U to be monotonically increasing: U(n+epsilon) gt U(n), where epsilon is positive.
+	///
+	/// As a result, only lg(n) can be used, where the basis of the log must be gt1.
+	/// we generally choose 2 as the log basis.
 	/// </summary>
 	/// <remarks>
 	/// <see cref="proffer.op_.binary_.FineTune"/> of m, n.
